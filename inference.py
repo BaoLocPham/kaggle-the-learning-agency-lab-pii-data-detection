10576b2dc8fa6a5e6b3b5179243a766efae0b760
BaoLocPham <phambaoloc163@gmail.com>
2024-02-26 16:47:47 +0700

    [FIX] max_length typo

--- inference.py
+++ inference.py
@@ -53,7 +53,7 @@ def tokenize(example, tokenizer, config):
         "".join(text),
         return_offsets_mapping=config.return_offsets_mapping,
         truncation=config.truncation,
-        max_length=config.max_length,
+        max_length=config.max_len,
         stride=config.stride,
         return_overflowing_tokens=config.return_overflowing_tokens
     )
